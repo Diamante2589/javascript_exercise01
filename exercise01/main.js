44159e05308bf208223e95336d3cbfd3762494ad
neberpa <nestorebernalpardo@gmail.com>
2023-06-07 14:51:50 -0700

    se crea el objeto para imc

---
+++ exercise01/main.js
@@ -0,0 +1,26 @@
+/*
+let height=prompt("ingrese su péso");
+let weight=prompt("ingrese tu pesos");
+let bodyMass= height*weight;
+
+console.log(bodyMass);
+
+*/
+
+
+let weight;
+let height;
+let bodyMass;
+
+const person = {
+    weight:prompt("ingrese su péso en kilos"),
+    height:prompt("ingrese tu peso en metros"),
+   
+
+
+};
+
+console.log( "el indice de masa corporral es " + person.weight * person.height);
+document.write("el indice de masa corporral es " + person.weight * person.height);
+
+
