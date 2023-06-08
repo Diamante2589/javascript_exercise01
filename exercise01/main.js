0ae20068a333234615a0c44d7d88f73d6a9d27eb
neberpa <nestorebernalpardo@gmail.com>
2023-06-08 13:57:55 -0700

    se agrega el cuadrado de altura

--- exercise01/main.js
+++ exercise01/main.js
@@ -7,20 +7,22 @@ console.log(bodyMass);
 
 */
 
-
+//se crea las variables
 let weight;
 let height;
-let bodyMass;
+let  bodyMass;
 
+//se crea un objeto person 
 const person = {
     weight:prompt("ingrese su péso en kilos"),
     height:prompt("ingrese tu peso en metros"),
-   
-
-
 };
 
-console.log( "el indice de masa corporral es " + person.weight * person.height);
-document.write("el indice de masa corporral es " + person.weight * person.height);
+//se crea un console.log que me muestre el peso de la persona y la altuta
+
+console.log( "el indice de masa corporal es " + person.weight / (person.height*person.height));
+
+//se crea un document.write que muestra el peso y la altura
+document.write("el indice de masa corporal es " + person.weight / (person.height*person.height));
 
 
